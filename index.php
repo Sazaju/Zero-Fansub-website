0e14721d6e5a5ec5dd84be72b653de37d3b7044a
Matthieu Vergne <sazaju@gmail.com>
2011-06-08 15:09:28 +0200

    Add some PHP code. Now there is only real PHP files.

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
+<?php
+	require_once("php/constants.php");
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
 	<head>
-		<title>Zero Fansub</title>
+		<title><?php echo TITLE; ?></title>
 		<!--
 			we give a "text/html" content to be compatible with most of the
-			explorers, it may be "application/xhtml+xml". See this link :
+			explorers, it should be "application/xhtml+xml". See this link :
 			
 			http://www.pompage.net/traduction/declarations
 		-->
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	</head>
 	<body>
-		<p>Here is a first file, no PHP code but it will come soon.</p>
+		<h1><?php echo TITLE; ?></h1>
+		<p>Here is a first file with some initial PHP code.</p>
 	</body>
 </html>
---
+++ php/constants.php
@@ -0,0 +1,9 @@
+<!--
+	This file gives all the constants used in the code. If a new value must be
+	added and seems to be not variable (typically some config values), it may
+	be defined here.
+-->
+
+<?php
+	define('TITLE', 'Zero Fansub');
+?>
