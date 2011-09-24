55f7d74f91337c5fbc889d1840e43acd43d24407
Matthieu VERGNE <matthieu.vergne@gmail.com>
2011-09-24 13:16:36 +0200

    Translate the 'contact' page in object PHP.

--- pages/contact.php
+++ pages/contact.php
@@ -1,10 +1,27 @@
-<h3>Contact</h3>
-<div class="p"><br/><br/><br/><br/>
-Un commentaire à faire ?<br />
-Une critique ?<br />
-Un lien mort ?<br />
-Une proposition ?<br />
-Un lien de streaming ?<br/><br/>
-
-Une seule adresse pour contacter la team :<br /><br /><br />
-<span style="font-size: 25px;"><a href="mailto:zero.fansub@gmail.com">zero.fansub@gmail.com</a></span></div>
+<?php
+	$title = new Title("Contact", 3);
+	$title->writeNow();
+	
+	$contact = new SimpleTextComponent();
+	$contact->addLine();
+	$contact->addLine();
+	$contact->addLine();
+	$contact->addLine();
+	$contact->addLine("Un commentaire à faire ?");
+	$contact->addLine("Une critique ?");
+	$contact->addLine("Un lien mort ?");
+	$contact->addLine("Une proposition ?");
+	$contact->addLine("Un lien de streaming ?");
+	$contact->addLine();
+	$contact->addLine("Une seule adresse pour contacter la team :");
+	$contact->addLine();
+	$contact->addLine();
+	$link = new MailLink("zero.fansub@gmail.com");
+	$link->setStyle("font-size: 25px;");
+	$contact->addComponent($link);
+	
+	$contact2 = new SimpleBlockComponent();
+	$contact2->setClass("p");
+	$contact2->addComponent($contact);
+	$contact2->writeNow();
+?>
