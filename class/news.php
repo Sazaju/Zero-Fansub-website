90f66ce951b35fdd13dabdf9c448fefbd0a1d94f
Matthieu Vergne <vergne@fbk.eu>
2011-09-27 18:16:47 +0200

    Add the twitter link to the last news.

--- class/news.php
+++ class/news.php
@@ -143,7 +143,7 @@ class News extends SimpleBlockComponent {
 			$news->setDate("26/09/2011");
 			$news->setAuthor(TeamMember::getMember(5));
 			$news->setCommentId(271);
-			$news->setTwitterUrl(null); // TODO
+			$news->setTwitterUrl("http://twitter.com/home?status=[Zero] Nouvelles sorties, nouveaux projets, nouveaux bugs..."); // TODO
 			$news->setMessage($newsMessage);
 			News::$allNews[] = $news;
 
