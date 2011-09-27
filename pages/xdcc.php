c8d031cadd4031a368286057bc3c702cc9340902
Matthieu Vergne <vergne@fbk.eu>
2011-09-27 16:50:24 +0200

    Testing auto update.

--- pages/xdcc.php
+++ pages/xdcc.php
@@ -15,5 +15,5 @@
 	$message->addLine("ou <code><i>NUMERO</i></code> doit &ecirc;tre remplac&eacute; par le num&eacute;ro du paquet (voir liste ci-dessous).");
 	$message->addLine();
 	$message->addLine();
-	$message->addLine("<iframe src='http://sazaju.dyndns-home.com/zero/lishiantus.txt' width='100%' height='600' frameborder='0'><p>Your browser does not support iframes.</p></iframe>");
-?>
+	$message->addLine("<iframe src='http://sazaju.dyndns-home.net/zero/lishiantus.txt' width='100%' height='600' frameborder='0'><p>Your browser does not support iframes.</p></iframe>");
+?>test
