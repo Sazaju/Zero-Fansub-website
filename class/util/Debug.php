a3db787e25c95c41e9c398044bec68feb2b5393c
Matthieu VERGNE <matthieu.vergne@gmail.com>
2011-10-04 00:48:45 +0200

    Review style managing + add convenient PHP features.

---
+++ class/util/Debug.php
@@ -0,0 +1,161 @@
+<?php
+class Debug {
+	public static function toString( $object, $name = 'object' ) {
+		
+		//définition de la variable contenant le texte
+		$string = '';
+		
+		//définition des couleurs d'affichage
+		$COLOR_OTHER	= 'green';	//couleur par défaut de la chaîne
+		$COLOR_QUOTE	= 'red';	//quotes dans les ['...'] des tableaux
+		
+		$COLOR_OBJECT	= 'blue';	//objets
+		$COLOR_ARRAY	= 'blue';	//tableaux
+		
+		$COLOR_BOOL		= 'black';	//booléen
+		$COLOR_DOUBLE	= 'black';	//flottants
+		$COLOR_INT		= 'black';	//entiers
+		$COLOR_STRING	= 'black';	//chaîne de caractères
+		$COLOR_NULL		= 'red';	//sans valeurs
+		$COLOR_RESOURCE	= 'red';	//resources (types extérieurs)
+		$COLOR_UNKNOWN	= 'red';	//autres
+		
+		//vérification si tableau non NULL (égalité souple pour prendre en compte un maximum de cas)
+		if ( $object == NULL ) {
+			$string .= '<font color="' . $COLOR_OTHER . '">';
+			$string .= '<b>$</b>' . $name;
+			$string .= ' = ';
+			$string .= '(<font color="' . $COLOR_NULL . '">NULL</font>)';
+			$string .= '</font>';
+			$string .= '<br />';
+		}
+		else {
+			//traitement de chaque entrée du tableau
+			foreach ( $object as $key => $value ) {
+				
+				//traitement de la clé
+				//création d'une variable donnant l'affichage formaté de la clé
+				//le formatage dépend du type de la clé
+				if ( is_array( $object ) ) {
+					$key_format = '<b>[</b>';
+					if ( is_int( $key ) ) {
+						$key_format .= '<font color="' . $COLOR_ARRAY . '">' . $key . '</font>';
+					}
+					else {
+						$key_format .= '<font color="' . $COLOR_ARRAY . '"><font color="' . $COLOR_QUOTE . '">\'</font>' . $key . '<font color="' . $COLOR_QUOTE . '">\'</font></font>';
+					}
+					$key_format .= '<b>]</b>';
+				}
+				elseif ( is_object( $object ) ) {
+					$key_format = '<b>-></b>';
+					$key_format .= '<font color="' . $COLOR_OBJECT . '">' . $key . '</font>';
+				}
+				else {
+					$key_format = '<b>-></b>';
+					$key_format .= '<font color="' . $COLOR_OTHER . '">' . $key . '</font>';
+				}
+				
+				//traitement de la valeur
+				//si c'est un tableau ou un objet, on relance la fonction dessus
+				//(sauf si c'est un rebouclage non souhaité)
+				//sinon on la considère comme une simple valeur et on l'affiche
+				if ( is_array( $value ) ) {
+					
+					//condition anti-rebouclage
+					if ( ( $key === $name ) && ( $key !== '0' ) ) {
+						$string .= '<font color="' . $COLOR_OTHER . '">';
+						$string .= '<b>$</b>' . $name;
+						$string .= $key_format . ' = ';
+						$string .= '<font color="' . $COLOR_ARRAY . '">array $' . $key . '</font>';
+						$string .= '</font>';
+						$string .= '<br />';
+					}
+					//traitement en tant que tableau
+					else {
+						$string .= Debug::toString( $value, $name . $key_format );
+					}
+				}
+				elseif ( is_object( $value ) ) {
+					
+					//condition anti-rebouclage
+					if ( ( $key === $name ) && ( $key !== '0' ) ) {
+						$string .= '<font color="' . $COLOR_OTHER . '">';
+						$string .= '<b>$</b>' . $name;
+						$string .= $key_format . ' = ';
+						$string .= '-><font color="' . $COLOR_OBJECT . '">object $' . $key . '</font>';
+						$string .= '</font>';
+						$string .= '<br />';
+					}
+					//traitement en tant qu'objet
+					else {
+						$string .= Debug::toString( $value, $name . $key_format );
+					}
+				}
+				//affichage de la valeur si ce n'est pas un tableau ou un objet
+				//l'affichage dépend du type de variable
+				else {
+					if ( is_bool( $value ) ) {
+						$string .= '<font color="' . $COLOR_OTHER . '">';
+						$string .= '<b>$</b>' . $name;
+						$string .= $key_format . ' = ';
+						$string .= '(bool) <font color="' . $COLOR_BOOL . '">' . ( $value ? 'TRUE' : 'FALSE' ) . '</font>';
+						$string .= '</font>';
+						$string .= '<br />';
+					}
+					elseif ( is_float( $value ) || is_double( $value ) ) {	//float et double sont strictement identique (double n'est qu'un alias de float)
+						$string .= '<font color="' . $COLOR_OTHER . '">';
+						$string .= '<b>$</b>' . $name;
+						$string .= $key_format . ' = ';
+						$string .= '(float) <font color="' . $COLOR_DOUBLE . '">' . $value . '</font>';
+						$string .= '</font>';
+						$string .= '<br />';
+					}
+					elseif ( is_int( $value ) ) {
+						$string .= '<font color="' . $COLOR_OTHER . '">';
+						$string .= '<b>$</b>' . $name;
+						$string .= $key_format . ' = ';
+						$string .= '(int) <font color="' . $COLOR_INT . '">' . $value . '</font>';
+						$string .= '</font>';
+						$string .= '<br />';
+					}
+					elseif ( is_string( $value ) ) {
+						$string .= '<font color="' . $COLOR_OTHER . '">';
+						$string .= '<b>$</b>' . $name;
+						$string .= $key_format . ' = ';
+						$string .= '(string['. strlen( $value ) .']) <font color="' . $COLOR_QUOTE . '">\'</font><font color="' . $COLOR_STRING . '"><pre style="display:inline;">' . $value . '</pre></font><font color="' . $COLOR_QUOTE . '">\'</font>';
+						$string .= '</font>';
+						$string .= '<br />';
+					}
+					elseif ( is_null( $value ) ) {
+						$string .= '<font color="' . $COLOR_OTHER . '">';
+						$string .= '<b>$</b>' . $name;
+						$string .= $key_format . ' = ';
+						$string .= '(<font color="' . $COLOR_NULL . '">NULL</font>)';
+						$string .= '</font>';
+						$string .= '<br />';
+					}
+					elseif ( is_resource( $value ) ) {
+						$string .= '<font color="' . $COLOR_OTHER . '">';
+						$string .= '<b>$</b>' . $name;
+						$string .= $key_format . ' = ';
+						$string .= '(resource) <font color="' . $COLOR_RESOURCE . '">' . $value . '(' . get_resource_type( $value ) . ')' . '</font>';
+						$string .= '</font>';
+						$string .= '<br />';
+					}
+					else {
+						$string .= '<font color="' . $COLOR_OTHER . '">';
+						$string .= '<b>$</b>' . $name;
+						$string .= $key_format . ' = ';
+						$string .= '(' . gettype( $value ) . ') <font color="' . $COLOR_UNKNOWN . '">unknowm</font>';
+						$string .= '</font>';
+						$string .= '<br />';
+					}
+				}
+			}
+		}
+		
+		//on renvoie le texte
+		return $string;
+	}
+}
+?>
---
+++ class/util/DirectoryManager.php
@@ -0,0 +1,139 @@
+<?php
+define("DM_SORT_NAME", "0");
+define("DM_SORT_TYPE", "1");
+define("DM_SORT_SIZE", "2");
+
+class DirectoryManager {
+    /*********************************************************************************\
+    Retourne un tableau listant le contenu du dossier, chaque élément possède les
+    informations suivantes :
+        - name      : nom de l'élément
+        - type      : 'file' pour les fichiers, 'directory' pour les dossiers,
+                      'unknown' pour le reste
+        - size      : taille en octets
+        - access    : droits d'accès (rwx en octal (0-7))
+        - content   : contenu du dossier (uniquement les dossiers et si $recursive = true)
+
+    L'argument $sorting est un tableau (clé => valeur) décrivant les tris à appliquer.
+	Chaque clé correspond a une des constantes suivantes :
+		- DM_SORT_NAME	: tri par nom
+		- DM_SORT_TYPE	: tri par type
+		- DM_SORT_SIZE	: tri par taille
+	Les valeurs associées à chaque clé décrivent le sens de tri est doivent valoir l'une
+	de ces constantes :
+		- SORT_ASC	: tri croissant
+		- SORT_DESC	: tri décroissant
+	Il est possible de demander plusieurs tris (jusqu'à 3). L'ordre des tris correspond
+	à celui fournit dans le tableau.
+    
+    \*********************************************************************************/
+    public static function getContent($path, $recursive = false, $sorting = array(), $ignore = array( '.', '..' )) {
+        $directory = @opendir($path);
+        if ( $directory === false ) {
+            throw new ErrorException( "Impossible d'ouvrir le dossier '$path'.", E_USER_ERROR );
+        }
+    
+		// safe formating
+		$lastIndex = strlen($path) - 1;
+        if ($path[$lastIndex] === '/') {
+            $path[$lastIndex] = '\0';
+        }
+
+		// browsing of the directory
+        $list = array();
+        while($file = readdir($directory)) {
+            if (!in_array($file, $ignore)) {
+                $element = array();
+                $file_path = $path.'/'.$file;
+    
+                $element['name'] = $file;
+    
+                if (is_file($file_path)) {
+                    $element['type'] = 'file';
+                }
+                elseif (is_dir($file_path)) {
+                    $element['type'] = 'directory';
+                    if ($recursive) {
+						$element['content'] = DirectoryManager::getContent($file_path, $recursive, $sorting, $ignore);
+					}
+                }
+                else {
+                    $element['type'] = 'unknown';
+                }
+    
+                $element['size'] = filesize($file_path);
+    
+                $element['access'] = 0;
+                $element['access'] += (is_readable( $file_path ) ? 4 : 0);
+                $element['access'] += (is_writable( $file_path ) ? 2 : 0);
+                $element['access'] += (is_executable( $file_path ) ? 1 : 0);
+    
+                $list[] = $element;
+            }
+        }
+        closedir($directory);
+    
+        // sorting
+        if (!empty($sorting)) {
+			// data formating
+            $sorting = str_split( $sorting );
+            $sort_order = array();
+            $last_sort = NULL;
+            foreach($sorting as $key => $direction) {
+				switch($key) {
+					case DM_SORT_NAME:
+						$last_sort = 'name';
+						break;
+					case DM_SORT_TYPE:
+						$last_sort = 'type';
+						break;
+					case DM_SORT_SIZE:
+						$last_sort = 'size';
+						break;
+					default:
+						throw new ErrorException("Bad formating of the sorting type: ".$key, E_USER_ERROR);
+				}
+				switch($direction) {
+					case SORT_ASC:
+					case SORT_DESC:
+						$sort_order[$last_sort] = $direction;
+						break;
+					default:
+						throw new ErrorException("Bad formating of the sorting direction: ".$direction, E_USER_ERROR);
+				}
+            }
+			
+			// sorting phase
+            if (!empty($sort_order)) {
+                $sort = array();
+                $sort_direction = array();
+                $i = 0;
+                foreach($sort_order as $key => $direction) {
+                    $sort_direction[$i] = $sort_order[$key];
+    
+                    foreach ($list as $num => $row) {
+                        $sort[$i][$num] = strtolower($row[$key]);
+                    }
+                    ++ $i;
+                }
+    
+                switch( $i ) {
+                    case 1 :
+                        array_multisort( $sort[0], $sort_direction[0], $list );
+                        break;
+                    case 2 :
+                        array_multisort( $sort[0], $sort_direction[0], $sort[1], $sort_direction[1], $list );
+                        break;
+                    case 3 :
+                        array_multisort( $sort[0], $sort_direction[0], $sort[1], $sort_direction[1], $sort[2], $sort_direction[2], $list );
+                        break;
+                    default :
+                }
+            }
+        }
+		
+		// sorted list
+        return $list;
+    }
+}
+?>
