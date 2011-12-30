363e604aa8af9b098348cbdaa8c8cae8074f19e2
Matthieu VERGNE <matthieu.vergne@gmail.com>
2011-12-30 18:06:08 +0100

    Refine Sketchbook Drama.

--- class/TeamMember.php
+++ class/TeamMember.php
@@ -458,6 +458,11 @@ class TeamMember {
 			$member->setPonctualMember(true);
 			TeamMember::$allMembers[] = $member;
 			
+			$member = new TeamMember(53);
+			$member->setPseudo("Basti3n");
+			$member->setPonctualMember(true);
+			TeamMember::$allMembers[] = $member;
+			
 			// TODO remove this sort, prefer a sort at a lower level
 			function sortMembers(TeamMember $a, TeamMember $b) {
 				return strcasecmp($a->getPseudo(), $b->getPseudo());
