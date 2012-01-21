e824fa1469ba648b347ecdd8f37072a8db3807c9
Matthieu VERGNE <matthieu.vergne@gmail.com>
2012-01-21 22:11:58 +0100

    When refuse to pass in H mode, return to the previous page if known.

--- pages/havert.php
+++ pages/havert.php
@@ -9,13 +9,19 @@
 	$page->addComponent($content);
 	
 	$content->addComponent(new Image("images/news/avert.jpg"));
-	$content->addComponent(new Title("Vous êtes sur le point d'entrer dans la zone Hentaï", 2));
-	$content->addComponent(new SimpleParagraphComponent("Comme son nom l'indique, cette partie regorge de machins dégoûtants interdits aux enfants. Mais bon, si vous êtes majeur, vacciné et consentant, on vous autorise à entrer là-dedans."));
+	$content->addComponent(new Title("Vous êtes sur le point d'entrer en mode Hentaï", 2));
+	$content->addComponent(new SimpleParagraphComponent("Comme son nom l'indique, vous vous apprêtez à regarder plein de machins dégoûtants interdits aux enfants. Mais bon, si vous êtes majeur, vacciné et consentant, on vous y autorise."));
+	
 	$okLink = new Link();
-	$okLink->setContent("Je rentre !");
+	$okLink->setContent("Montrer les machins dégoutants");
 	$url = $okLink->getUrl();
 	$url->removeQueryVar(DISPLAY_H_AVERT);
 	$url->setQueryVar(DISPLAY_H, true);
 	$content->addComponent($okLink);
-	$content->addComponent(new Link(Url::indexUrl(), "Je sors..."));
+	
+	$cancelLink = new Link(Url::indexUrl(), "Garder mon écran propre");
+	if (isset($_SERVER["HTTP_REFERER"])) {
+		$cancelLink->setUrl(new Url($_SERVER["HTTP_REFERER"]));
+	}
+	$content->addComponent($cancelLink);
 ?>
