f3707df77a9761784cfbc607e62f1f0cf8da7287
Matthieu VERGNE <matthieu.vergne@gmail.com>
2012-02-05 18:07:29 +0100

    Refine kanaiiddl page.

--- pages/kanaiiddl.php
+++ pages/kanaiiddl.php
@@ -1,203 +1,88 @@
-
-<h3>Kanaii DDL</h3>
-<p><a href="http://www.kanaii.com" target="_blank"><img src="images/kanaii/kanaii.jpg" border="0"></a>
-
-<div style="float : right; display:block; margin-right: 20px;">
-	<img src="images/kanaii/kamen.jpg" border="0">
-</div>
-<h2>Kamen no maid guy</h2>
-<h4>Comédie</h4>
-
-
-<ul>
-<li>	Episode 01v2 
-		[ <a href="ddl/kanaii/[Kanaii]_Kamen_No_Maid_Guy_01v2_[X264_1280x720].mkv">Télécharger</a> ]  
-		</li>
-<li>	Episode 02 
-		[ <a href="ddl/kanaii/[Kanaii]_Kamen_No_Maid_Guy_02_[X264_1280x720].mkv">Télécharger</a> ]  
-		</li>
-<li>	Episode 03 
-		[ <a href="ddl/kanaii/[Kanaii]_Kamen_No_Maid_Guy_03_[X264_1280x720].mkv">Télécharger</a> ]  
-		</li>
-		<li>	Episode 04 
-		[ <a href="ddl/kanaii/[Kanaii]_Kamen_No_Maid_Guy_04_[X264_1280x720].mkv">Télécharger</a> ]  
-		</li>
-		<li>	Episode 05 
-		[ <a href="ddl/kanaii/[Kanaii]_Kamen_No_Maid_Guy_05_[X264_1280x720].mkv">Télécharger</a> ]  
-		</li>
-		<li>	Episode 06 
-		[ <a href="ddl/kanaii/[Kanaii]_Kamen_No_Maid_Guy_06_[X264_1280x720].mkv">Télécharger</a> ]  
-		</li>
-		<li>	Episode 07 
-		[ <a href="ddl/kanaii/[Kanaii]_Kamen_No_Maid_Guy_07_[X264_1280x720].mkv">Télécharger</a> ]  
-		</li>
-		<li>	Episode 08 
-		[ <a href="ddl/kanaii/[Kanaii]_Kamen_No_Maid_Guy_08_[X264_1280x720].mkv">Télécharger</a> ]  
-		</li>
-		<li>	Episode 09 
-		[ <a href="ddl/kanaii/[Kanaii]_Kamen_No_Maid_Guy_09_[X264_1280x720].mkv">Télécharger</a> ]  
-		</li>
-		<li>	Episode 10 
-		[ <a href="ddl/kanaii/[Kanaii]_Kamen_No_Maid_Guy_10_[X264_1280x720].mkv">Télécharger</a> ]  
-		</li>
-		<li>	Episode 11 
-		[ <a href="ddl/kanaii/[Kanaii]_Kamen_No_Maid_Guy_11_[X264_1280x720].mkv">Télécharger</a> ]  
-		</li>
-<li class="nd">	Episode 12 [ Non disponible ]</li>
-</ul>
-
-
-<p style="text-align: center;">
-<a href="http://www.kanaii.com" target="_blank">Site officiel de la Kanaii</a></p>
-
-
-
-<div style="float : left; display:block; margin-right: 20px; margin-left: 20px;">
-	<img src="images/kanaii/kanokon.jpg" border="0">
-</div>
-<h2>Kanokon</h2>
-<h4>Comédie - Ecchi</h4>
-
-
-<ul style="text-align: right;">
-		<li>	Episode 01 
-		[ <a href="ddl/kanaii/[Kanaii]_Kanokon_01_DVD_[848x480_X264].mkv">Télécharger</a> ]  
-		</li>
-				<li>	Episode 02 
-		[ <a href="ddl/kanaii/[Kanaii]_Kanokon_02_DVD_[848x480_X264].mkv">Télécharger</a> ]  
-		</li>
-				<li>	Episode 03 
-		[ <a href="ddl/kanaii/[Kanaii]_Kanokon_03_DVD_[848x480_X264].mkv">Télécharger</a> ]  
-		</li>
-				<li>	Episode 04 
-		[ <a href="ddl/kanaii/[Kanaii]_Kanokon_04_DVD_[848x480_X264].mkv">Télécharger</a> ]  
-		</li>
-				<li>	Episode 05 
-		[ <a href="ddl/kanaii/[Kanaii]_Kanokon_05_DVD_[848x480_X264].mkv">Télécharger</a> ]  
-		</li>
-				<li>	Episode 06 
-		[ <a href="ddl/kanaii/[Kanaii]_Kanokon_06_DVD_[848x480_X264].mkv">Télécharger</a> ]  
-		</li>
- <li class="nd">	Episode 07 [ Non disponible ]</li>
-<li class="nd">	Episode 08 [ Non disponible ]</li>
-<li class="nd">	Episode 09 [ Non disponible ]</li>
-<li class="nd">	Episode 10 [ Non disponible ]</li>
-<li class="nd">	Episode 11 [ Non disponible ]</li>
-<li class="nd">	Episode 12 [ Non disponible ]</li>
-		</li>
-		<li>	Bonus DVD 1 
-		[ <a href="ddl/kanaii/[Kanaii]_Kanokon_DVD1_Bonus1_[X264_848x480].mkv">Télécharger</a> ]  
-		</li>
-			<li>	Bonus DVD 2 
-		[ <a href="ddl/kanaii/[Kanaii]_Kanokon_DVD1_Bonus2_[X264_848x480].mkv">Télécharger</a> ]  
-		</li>
-</ul>
-
-<p style="text-align: center;">
-<a href="http://www.kanaii.com" target="_blank">Site officiel de la Kanaii</a></p>
-
-
-
-
-
-
-
-
-
-
-<div style="float : right; display:block; margin-right: 20px; margin-left: 20px;">
-	<img src="images/kanaii/kimi.jpg" border="0">
-</div>
-<h2>Kimi ga Aruji de Shitsuji ga Ore de - They are my Noble Masters</h2>
-<h4>Comédie - Ecchi</h4>
-
-
-<ul>
-		<li>	Episode 01 
-		[ <a href="ddl/kanaii/[GF-Kanaii]_Kimi_ga_Aruji_de_Shitsuji_ga_Ore_de_-_MQ_01_[XviD][EDCFBE8A].avi">Télécharger</a> ]  
-		</li>
-				<li>	Episode 02 
-		[ <a href="ddl/kanaii/[GF-Kanaii]_Kimi_ga_Aruji_de_Shitsuji_ga_Ore_de_-_MQ_02_[XviD][D468AAA6].avi">Télécharger</a> ]  
-		</li>
-<li class="nd">	Episode 03 [ Non disponible ]</li>
-<li class="nd">	Episode 04 [ Non disponible ]</li>
-<li class="nd">	Episode 05 [ Non disponible ]</li>
-<li class="nd">	Episode 06 [ Non disponible ]</li>
-<li class="nd">	Episode 07 [ Non disponible ]</li>
-<li class="nd">	Episode 08 [ Non disponible ]</li>
-<li class="nd">	Episode 09 [ Non disponible ]</li>
-<li class="nd">	Episode 10 [ Non disponible ]</li>
-<li class="nd">	Episode 11 [ Non disponible ]</li>
-<li class="nd">	Episode 12 [ Non disponible ]</li>
-<li class="nd">	Episode 13 [ Non disponible ]</li>
-</ul>
-
-<p style="text-align: center;">
-<a href="http://www.kanaii.com" target="_blank">Site officiel de la Kanaii</a></p>
-
-
-
-
-
-
-
-
-
-
-<div style="float : right; display:block; margin-right: 20px; margin-left: 20px;">
-	<img src="images/kanaii/naga.jpg" border="0">
-	</div>
-<h2>Nagasarete Airantô</h2>
-<h4>Comédie - Ecchi</h4>
-
-
-<ul>
-<li>	Episode 01 
-		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_01v2_[1280x720_X264].mkv">Télécharger</a> ]  
-		</li>
-<li>	Episode 02 
-		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_02v2_[1280x720_X264].mkv">Télécharger</a> ]  
-		</li>
-<li>	Episode 03 
-		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_03v2_[1280x720_X264].mkv">Télécharger</a> ]  
-		</li>
-
-		<li>	Episode 04 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_04v2_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-		<li>	Episode 05 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_05_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-		<li>	Episode 06 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_06_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-
-		<li>	Episode 07 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_07_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-		<li>	Episode 08 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_08_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-		<li>	Episode 09 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_09_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-
-		<li>	Episode 10 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_10v2_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-		<li>	Episode 11 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_11_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-		<li>	Episode 12 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_12_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-
-		<li>	Episode 13 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_13_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-		<li>	Episode 14 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_14_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-		<li>	Episode 15 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_15_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-
-		<li>	Episode 16 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_16_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-		<li>	Episode 17 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_17_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-		<li>	Episode 18 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_18_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-
-		<li>	Episode 19 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_19_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-		<li>	Episode 20 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_20_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-		<li>	Episode 21 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_21_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-
-		<li>	Episode 22 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_22_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-		<li>	Episode 23 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_23_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-		<li>	Episode 24 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_24_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-
-		<li>	Episode 25 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_25v2_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-		<li>	Episode 26 		[ <a href="ddl/kanaii/[Kanaii]Nagasarete_Airantou_26_[1280x720_X264].mkv">Télécharger</a> ]  </li>
-</ul>
-
-
-<p style="text-align: center;">
-<a href="http://www.kanaii.com" target="_blank">Site officiel de la Kanaii</a></p>
-
-
-
-
+<?php
+	$content = "[title]Kanaii DDL[/title]
+[partner=kanaii][img]images/kanaii/kanaii.jpg[/img][/partner]
+[separator]
+[imgr]images/kanaii/kamen.jpg[/imgr][title=2]Kamen no maid guy[/title][title=4]Comédie[/title][list]
+[item]Episode 01v2 [ [url=ddl/kanaii/%5BKanaii%5D_Kamen_No_Maid_Guy_01v2_%5BX264_1280x720%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 02 [ [url=ddl/kanaii/%5BKanaii%5D_Kamen_No_Maid_Guy_02_%5BX264_1280x720%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 03 [ [url=ddl/kanaii/%5BKanaii%5D_Kamen_No_Maid_Guy_03_%5BX264_1280x720%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 04 [ [url=ddl/kanaii/%5BKanaii%5D_Kamen_No_Maid_Guy_04_%5BX264_1280x720%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 05 [ [url=ddl/kanaii/%5BKanaii%5D_Kamen_No_Maid_Guy_05_%5BX264_1280x720%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 06 [ [url=ddl/kanaii/%5BKanaii%5D_Kamen_No_Maid_Guy_06_%5BX264_1280x720%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 07 [ [url=ddl/kanaii/%5BKanaii%5D_Kamen_No_Maid_Guy_07_%5BX264_1280x720%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 08 [ [url=ddl/kanaii/%5BKanaii%5D_Kamen_No_Maid_Guy_08_%5BX264_1280x720%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 09 [ [url=ddl/kanaii/%5BKanaii%5D_Kamen_No_Maid_Guy_09_%5BX264_1280x720%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 10 [ [url=ddl/kanaii/%5BKanaii%5D_Kamen_No_Maid_Guy_10_%5BX264_1280x720%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 11 [ [url=ddl/kanaii/%5BKanaii%5D_Kamen_No_Maid_Guy_11_%5BX264_1280x720%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 12 [ Non disponible ][/item]
+[/list]
+[partner=kanaii]Site officiel de la Kanaii[/partner]
+[separator]
+[imgr]images/kanaii/kanokon.jpg[/imgr][title=2]Kanokon[/title][title=4]Comédie - Ecchi[/title][list]
+[item]Episode 01 [ [url=ddl/kanaii/%5BKanaii%5D_Kanokon_01_DVD_%5B848x480_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 02 [ [url=ddl/kanaii/%5BKanaii%5D_Kanokon_02_DVD_%5B848x480_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 03 [ [url=ddl/kanaii/%5BKanaii%5D_Kanokon_03_DVD_%5B848x480_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 04 [ [url=ddl/kanaii/%5BKanaii%5D_Kanokon_04_DVD_%5B848x480_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 05 [ [url=ddl/kanaii/%5BKanaii%5D_Kanokon_05_DVD_%5B848x480_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 06 [ [url=ddl/kanaii/%5BKanaii%5D_Kanokon_06_DVD_%5B848x480_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 07 [ Non disponible ][/item]
+[item]Episode 08 [ Non disponible ][/item]
+[item]Episode 09 [ Non disponible ][/item]
+[item]Episode 10 [ Non disponible ][/item]
+[item]Episode 11 [ Non disponible ][/item]
+[item]Episode 12 [ Non disponible ][/item]
+[item]Bonus DVD 1 [ [url=ddl/kanaii/%5BKanaii%5D_Kanokon_DVD1_Bonus1_%5BX264_848x480%5D.mkv]Télécharger[/url] ][/item]
+[item]Bonus DVD 2 [ [url=ddl/kanaii/%5BKanaii%5D_Kanokon_DVD1_Bonus2_%5BX264_848x480%5D.mkv]Télécharger[/url] ][/item]
+[/list]
+[partner=kanaii]Site officiel de la Kanaii[/partner]
+[separator]
+[imgr]images/kanaii/kimi.jpg[/imgr][title=2]Kimi ga Aruji de Shitsuji ga Ore de - They are my Noble Masters[/title][title=4]Comédie - Ecchi[/title][list]
+[item]Episode 01 [ [url=ddl/kanaii/%5BGF-Kanaii%5D_Kimi_ga_Aruji_de_Shitsuji_ga_Ore_de_-_MQ_01_%5BXviD%5D%5BEDCFBE8A%5D.avi]Télécharger[/url] ][/item]
+[item]Episode 02 [ [url=ddl/kanaii/%5BGF-Kanaii%5D_Kimi_ga_Aruji_de_Shitsuji_ga_Ore_de_-_MQ_02_%5BXviD%5D%5BD468AAA6%5D.avi]Télécharger[/url] ][/item]
+[item]Episode 03 [ Non disponible ][/item]
+[item]Episode 04 [ Non disponible ][/item]
+[item]Episode 05 [ Non disponible ][/item]
+[item]Episode 06 [ Non disponible ][/item]
+[item]Episode 07 [ Non disponible ][/item]
+[item]Episode 08 [ Non disponible ][/item]
+[item]Episode 09 [ Non disponible ][/item]
+[item]Episode 10 [ Non disponible ][/item]
+[item]Episode 11 [ Non disponible ][/item]
+[item]Episode 12 [ Non disponible ][/item]
+[item]Episode 13 [ Non disponible ][/item]
+[/list]
+[partner=kanaii]Site officiel de la Kanaii[/partner]
+[separator]
+[imgr]images/kanaii/naga.jpg[/imgr][title=2]Nagasarete Airantô[/title][title=4]Comédie - Ecchi[/title][list]
+[item]Episode 01 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_01v2_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 02 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_02v2_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 03 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_03v2_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 04 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_04v2_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 05 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_05_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 06 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_06_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 07 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_07_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 08 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_08_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 09 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_09_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 10 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_10v2_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 11 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_11_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 12 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_12_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 13 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_13_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 14 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_14_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 15 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_15_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 16 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_16_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 17 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_17_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 18 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_18_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 19 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_19_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 20 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_20_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 21 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_21_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 22 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_22_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 23 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_23_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 24 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_24_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 25 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_25v2_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[item]Episode 26 [ [url=ddl/kanaii/%5BKanaii%5DNagasarete_Airantou_26_%5B1280x720_X264%5D.mkv]Télécharger[/url] ][/item]
+[/list]
+[partner=kanaii]Site officiel de la Kanaii[/partner]
+[separator]
+";
+	PageContent::getInstance()->addComponent(Format::convertTextToHTML($content));
+?>
