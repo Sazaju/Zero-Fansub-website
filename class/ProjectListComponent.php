dd08058aa28653585e3bc8261d14c4df631fcd75
Matthieu VERGNE <matthieu.vergne@gmail.com>
2012-02-05 01:15:08 +0100

    Finish to refine the data of the obsolete page of hentai projects.

---
+++ class/ProjectListComponent.php
@@ -0,0 +1,37 @@
+<?php
+class ProjectListComponent extends SimpleListComponent {
+	private $useImage = false;
+	
+	public function __construct(ProjectList $list = null) {
+		$this->updateClass();
+		if ($list !== null) {
+			foreach($list->getProjects() as $project) {
+				$this->addComponent(new ProjectLink($project));
+			}
+		}
+	}
+	
+	public function addComponent($project) {
+		if ($project instanceof ProjectLink) {
+			$project->useImage($this->useImage);
+			parent::addComponent($project);
+		}
+		else {
+			throw new Exception("Cannot take components other than project links.");
+		}
+	}
+	
+	public function useImage($boolean) {
+		$this->useImage = $boolean;
+		foreach($this->getComponents() as $listElement) {
+			$projectLink = $listElement->getComponent(0);
+			$projectLink->useImage($this->useImage);
+		}
+		$this->updateClass();
+	}
+	
+	private function updateClass() {
+		$this->setClass("projectList".($this->useImage ? "Image" : "Text"));
+	}
+}
+?>
--- class/projectList.php
+++ class/projectList.php
@@ -3,7 +3,7 @@
 	A project list is a list able to take the projects of the team and format it in a convenient way.
 */
 
-class ProjectList extends SimpleListComponent {
+class ProjectList {
 	private $projects = array();
 	
 	public function addProject(Project $project) {
@@ -16,42 +16,6 @@ class ProjectList extends SimpleListComponent {
 	
 	public function sortByNames() {
 		usort($this->projects, array('Project', 'nameSorter'));
-	}
-}
-
-class ProjectListComponent extends SimpleListComponent {
-	private $useImage = false;
-	
-	public function __construct(ProjectList $list = null) {
-		$this->updateClass();
-		if ($list !== null) {
-			foreach($list->getProjects() as $project) {
-				$this->addComponent(new ProjectLink($project));
-			}
-		}
-	}
-	
-	public function addComponent($project) {
-		if ($project instanceof ProjectLink) {
-			$project->useImage($this->useImage);
-			parent::addComponent($project);
-		}
-		else {
-			throw new Exception("Cannot take components other than project links.");
-		}
-	}
-	
-	public function useImage($boolean) {
-		$this->useImage = $boolean;
-		foreach($this->getComponents() as $listElement) {
-			$projectLink = $listElement->getComponent(0);
-			$projectLink->useImage($this->useImage);
-		}
-		$this->updateClass();
-	}
-	
-	private function updateClass() {
-		$this->setClass("projectList".($this->useImage ? "Image" : "Text"));
 	}
 }
 ?>
