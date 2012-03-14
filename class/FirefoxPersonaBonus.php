5be10c50fc2875a49e3848cd96b9b1e5b6c9a482
Matthieu VERGNE <matthieu.vergne@gmail.com>
2012-03-14 17:26:09 +0100

    Create and apply a template for Firefox Personas bonuses.

---
+++ class/FirefoxPersonaBonus.php
@@ -0,0 +1,24 @@
+<?php
+class FirefoxPersonaBonus extends ProjectBonus {
+	public function __construct($ids, $description = null) {
+		parent::__construct("Thèmes pour Firefox (Skin Persona)", FirefoxPersonaBonus::buildContent($ids, $description));
+	}
+	
+	private static function buildContent($ids, $description) {
+		if (!is_array($ids)) {
+			throw new Exception("IDs and descriptions has to be given in an array");
+		} else {
+			$rootLink = 'http://www.getpersonas.com/en-US/persona/';
+			$rootPicture = 'http://getpersonas-cdn.mozilla.net/static/';
+			
+			$links = "";
+			foreach($ids as $id) {
+				$id = "$id";
+				$pathPicture = $id[strlen($id)-2].'/'.$id[strlen($id)-1].'/';
+				$links .= '<a target="_blank" href="'.$rootLink.$id.'"><img src="'.$rootPicture.$pathPicture.$id.'/preview.jpg" alt="'.$description.'" /></a>';
+			}
+			return $links;
+		}
+	}
+}
+?> 
