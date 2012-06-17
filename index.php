f700beff99dfcc278adabba83e216b872e654699
Sazaju HITOKAGE <sazaju@gmail.com>
2012-06-17 02:12:39 +0200

    Reuse the strict doctype instead of transitional (otherwise there is display changes).

--- index.php
+++ index.php
@@ -232,7 +232,7 @@ if ($url->hasQueryVar('page')) {
 }
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 	<head>
 		<title>Zéro ~fansub~ :: Le Site Officiel <?php echo WEBSITE_VERSION?></title>
