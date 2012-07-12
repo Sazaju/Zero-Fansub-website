3ef0d7afb7d26a1781de787e831d1eb44b9b113b
Matthieu VERGNE <matthieu.vergne@gmail.com>
2012-07-12 03:52:12 +0200

    Add a base class for patch instructions.

--- class/model/database/PatchManager.php
+++ class/model/database/PatchManager.php
@@ -126,4 +126,45 @@ class PatchManager {
 		}
 	}
 }
-?> 
+
+/*************************************\
+              PATCH BASE
+\*************************************/
+
+abstract class PatchInstruction {
+	abstract protected function getRegex();
+	abstract protected function applyValue($value);
+	
+	private $value;
+	public function setValue($value) {
+		if ($this->isSyntaxicallyCompatible($value)) {
+			$this->value = $value;
+			$this->applyValue($value);
+		} else {
+			throw new Exception("Incompatible value for ".get_class($this).": $value");
+		}
+	}
+	
+	public function getValue() {
+		return $this->value;
+	}
+	
+	public static function formatRegex($regex, $delimiter) {
+		if (strlen($delimiter) > 1) {
+			throw new Exception("The delimiter should be a single character");
+		} else if ($delimiter == ' ' || $delimiter == '\\') {
+			throw new Exception("The delimiter cannot be a space nor a backslash");
+		} else {
+			return str_replace($delimiter, '\\'.$delimiter, $regex);
+		}
+	}
+	
+	public function getFormattedRegex($delimiter) {
+		return PatchInstruction::formatRegex($this->getRegex(), $delimiter);
+	}
+	
+	public function isSyntaxicallyCompatible($value) {
+		return preg_match('#^'.$this->getFormattedRegex('#').'$#s', $value);
+	}
+}
+?>
