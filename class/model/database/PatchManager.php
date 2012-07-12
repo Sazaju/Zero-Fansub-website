21fe2d207bc14254bbe35582d55710b70e911793
Matthieu VERGNE <matthieu.vergne@gmail.com>
2012-07-12 18:44:25 +0200

    Add AlternativePatchInstruction (... OR ...).

--- class/model/database/PatchManager.php
+++ class/model/database/PatchManager.php
@@ -504,4 +504,59 @@ class OptionalPatchInstruction extends RepetitivePatchInstruction {
 		return array_shift($instructions);
 	}
 }
+
+class AlternativePatchInstruction extends PatchInstruction {
+	private $alternatives;
+	private $compatibleInstruction;
+	
+	public function __construct() {
+		$alternatives = array();
+		foreach(func_get_args() as $element) {
+			if ($element instanceof PatchInstruction) {
+				$alternatives[] = $element;
+			} else {
+				throw new Exception("$element is not managed in alternative instructions");
+			}
+		}
+		$this->alternatives = $alternatives;
+	}
+	
+	function __clone() {
+		$alternatives = array();
+		foreach($this->alternatives as $element) {
+			$alternatives[] = clone $element;
+		}
+		$this->alternatives = $alternatives;
+		$this->compatibleInstruction = $this->compatibleInstruction === null ? null : clone $this->compatibleInstruction;
+	}
+	
+	public function getAlternatives() {
+		return $this->alternatives;
+	}
+	
+	protected function getRegex() {
+		$globaleRegex = "";
+		foreach($this->alternatives as $instruction) {
+			$regex = $instruction->getRegex();
+			$globaleRegex .= "|(?:$regex)";
+		}
+		$globaleRegex = substr($globaleRegex, 1);
+		return "(?:$globaleRegex)";
+	}
+	
+	protected function applyValue($value) {
+		foreach($this->alternatives as $instruction) {
+			if ($instruction->isSyntaxicallyCompatible($value)) {
+				$instruction->setValue($value);
+				$this->compatibleInstruction = $instruction;
+			} else {
+				continue;
+			}
+		}
+	}
+	
+	public function getCompatibleInstruction() {
+		return $this->compatibleInstruction;
+	}
+}
 ?>
