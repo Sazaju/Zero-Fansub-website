e1413b4256246f19c964b444bb3980153468462d
Sazaju HITOKAGE <sazaju@gmail.com>
2012-07-25 20:16:39 +0200

    Improve the Twitter button data.

--- class/view/NewsComponent.php
+++ class/view/NewsComponent.php
@@ -87,7 +87,7 @@ class NewsComponent extends SimpleBlockComponent {
 		if ($twitterTitle == null) {
 			$twitterTitle = "[Zero] ".$news->getTitle();
 		}
-		$twitterButton = '<a href="https://twitter.com/share" class="twitter-share-button" data-text="'.$twitterTitle.'" data-lang="fr" data-hashtags="zerofansub">Tweeter</a>
+		$twitterButton = '<a href="https://twitter.com/share" class="twitter-share-button" data-url="'.$newsUrl->toFullString().'" data-text="'.$twitterTitle.'" data-via="zero_fansub" data-lang="fr">Tweeter</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
 		$this->addComponent($twitterButton);
 		
