cfd479e19a5d478acda0c87b631bd0ea5d4cccc9
Sazaju HITOKAGE <sazaju@gmail.com>
2012-12-31 16:40:24 +0100

    Update the v3.6 news (commented).

--- class/model/News.php
+++ class/model/News.php
@@ -4476,21 +4476,39 @@ Cela dit, ce n'est pas parce que l'année 2012 est finie que nos besoins le sont
 /*
 			$news = new News();
 			$news->setTitle("Mise à jour du site v3.6");
-			$news->setPublicationTime(strtotime("11 November 2012 12:51"));
+			$news->setPublicationTime(strtotime("31 December 2012 15:56"));
 			$news->setAuthor(TeamMember::getMemberByPseudo('Sazaju HITOKAGE'));
-			$news->setMessage("
-[spoiler=<Montrer la liste>]
+			$news->setMessage("Depuis la v3.5, plusieurs petites évolutions ont eu lieu. Comme d'habitude, peu d'influence sur le rendu, surtout une refonte du code interne.
+
+[spoiler=Cliquez ici pour montrer les changements.]
 [left][list]
-[item]Renouvellement des flux RSS (les plus attentifs devraient l'avoir remarqué depuis longtemps) avec la possibilité de les paramétrer : l'icône RSS que vous pouvez voir à côté de \"Vues\" vous permet d'accéder au flux RSS de la vue courante, il existe aussi de quoi contrôler la taille de la liste. Je ne détaille pas car ça peut encore changer (reportez-vous au code sur GitHub pour les plus curieux, ou demandez tout simplement {^_^}).[/item]
-[item][/item]
-[item][/item]
-[item][/item]
-[item][/item]
-[item][/item]
-[item]Autres nettoyages de code, corrections de bugs et trucs du genre[/item]
+	[item]Renouvellement des flux RSS (les plus attentifs devraient l'avoir remarqué depuis longtemps) avec la possibilité de les paramétrer : l'icône RSS que vous pouvez voir à côté de \"Vues\" vous permet d'accéder au flux RSS de la vue courante, il existe aussi de quoi contrôler la taille de la liste. Je ne détaille pas car ça peut encore changer (reportez-vous au code sur GitHub pour les plus curieux, ou demandez tout simplement {^_^}).[/item]
+	[item]Revue de la page à propos pour intégrer les dernières versions du site (avec une mise à jour automatique... normalement).[/item]
+	[item]Ajout du style pour Noël (avec changement annuel automatique).[/item]
+	[item]Extension de la fonctionnalité de programmation : on pouvait préparer les news pour qu'elles s'affichet à une heure donnée, on peut faire de même avec les releases et projets maintenant.[/item]
+	[item]Plusieurs corrections orthographiques/grammaticales dans nos séries, corrections de liens morts, etc.[/item]
+	[item]Autres nettoyages de code, corrections de bugs et trucs du genre.[/item]
 [/list][/left]
 [/spoiler]
-");
+
+J'en profite pour vous faire savoir que je cherche quelqu'un pour m'aider à programmer tout ça. En effet j'ai un paquet de tâches à faire, dont certaines grosses tâches et un paquet de nouveautés (reconnaissance de liens morts, liens magnet, chat IRC, apprentissage et entraînement fansub, etc.). Je prends pour exemple notre branche pour la v4 (utilisation d'une base de données, pour pouvoir se connecter et faire plein de trucs directement sur le site) qui existe depuis un moment et est bien avancé, mais que je n'ai pas touché depuis un bail, faute de temps.
+
+[spoiler=Cliquez ici pour voir ce que je recherche.][left]
+Le candidat doit :
+[list]
+	[item]Avoir du temps à consacrer, je ne cherche pas quelqu'un d'aussi surbooké que moi sinon ça n'a aucun intérêt.[/item]
+	[item]Aimer programmer, et cela de manière claire, efficace et maintenable.[/item]
+	[item]Connaître PHP et avoir des notions en SQL.[/item]
+	[item]Connaître ou avoir envie d'utiliser Git et GitHub (bases suffisantes, inutile d'être un expert).[/item]
+	[item]Être prêt à apprendre et faire des choses qu'il ne maîtrise pas du tout, quitte à recommencer plusieurs fois jusqu'à ce que ça marche du feu de Dieu.[/item]
+[/list]
+Le dernier point, bien que ça semble être du bon sens, est plus profond. En effet, je fais de la recherche en informatique (IRL), et donc j'utilise des techniques qui sont à l'état de l'art (façon compliquée de dire [i]nouvelles[/i]) et n'hésite pas à revoir depuis zéro des principes de base pour faire des choses plus optimisées. Il est donc important que le candidat n'ait pas froid aux yeux.
+
+Mais soyez rassurés, je serai derrière vous et toutes les modifs passeront par moi, donc pas de soucis pour ceux qui craindraient faire des bétises. Vous n'aurez aucune influence directe sur le site. De même, vous travaillez sur votre propre copie du site, donc vous pouvez essayer tout et n'importe quoi, comme ça vous chante. Enfin, on commencera avec des tâches simples (j'en ai un paquet) dire de prendre en main le code avant de passer aux choses sérieuses {^_°}.
+
+Une autre précision est que, bien que je recherche un candidat, n'importe qui peut participer (le site est en [url=https://github.com/Sazaju/Zero-Fansub-website]open source sur GitHub[/url]). C'est juste que je cherche quelqu'un qui s'impliquera de manière plus active et qui sera donc régulièrement présent sur le forum, en tant que membre de la team. Il peut bien entendu postuler pour une tâche de fansub si le cœur lui en dit, mais ça ne sera pas sa tâche principale. J'ai pas mal de tâches à faire, dont certaines sont assez conséquentes. Il y a de quoi s'amuser pour un bon moment {^_^}.
+[/left]
+[/spoiler]");
 			$news->setCommentId(302);
 			$news->setDisplayInNormalMode(true);
 			$news->setDisplayInHentaiMode(true);
