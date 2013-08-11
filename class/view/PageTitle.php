c6c95163aca6269521b7911a46b8f2e7db079700
Sazaju HITOKAGE <sazaju@gmail.com>
2013-08-11 21:22:39 +0200

    Remove unaligned description.

--- class/view/PageTitle.php
+++ class/view/PageTitle.php
@@ -1,10 +1,4 @@
 <?php
-/*
-	A pin is an invisible element allowing to fix some problematic points with the
-	floating CSS positioning. As its code may not change, the generating of the
-	HTML code is already done after the instanciation.
-*/
-
 class PageTitle extends DefaultHtmlComponent {
 	function __construct($title) {
 		$this->setClass('pageTitle');
