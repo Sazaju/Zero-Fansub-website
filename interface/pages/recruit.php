52197658d28e4642a4f3380a9e8ccdeb93ebf540
Sazaju HITOKAGE <sazaju@gmail.com>
2013-10-13 20:40:39 +0200

    Refine the recruit page considering previous experience.

--- interface/pages/recruit.php
+++ interface/pages/recruit.php
@@ -6,16 +6,14 @@
 [left]
 [title=2|number]Prendre connaissance des engagements[/title]
 
-    * Avoir plus de 16 ans
+    * Avoir de préférence plus de 16 ans
     * Avoir lu les [url=http://forum.zerofansub.net/t552-Les-regles-d-Or-de-chez-Zero.htm]règles d'Or[/url]
-    * Te tenir aux engagements que tu auras précisés dans ta candidature
-    * Ne pas mentir ou gonfler tes exploits dans ta candidature
-    * Rejoindre nos rangs si ta candidature est acceptée
-    * Ne pas insulter, provoquer, menacer, s'énerver si celle-ci est refusée
-    * Être disponible ou préciser à quels moments tu seras indisponible
-    * Éviter de rester inactif trop longtemps
-    * Ne pas bâcler ton travail une fois accepté
-    * Respecter les autres membres et apprendre d'eux
+    * Se tenir aux engagements qu'on aura précisé dans sa candidature
+    * Ne pas mentir ou gonfler/réduire ses exploits dans sa candidature
+    * Accepter dans la bonne humeur le résultat de la candidature, qu'elle soit acceptée ou refusée
+    * Être actif ou faire savoir dès que possible quand on pense s'absenter un certain temps
+    * Favoriser la qualité à la rapidité (mieux vaut avoir un travail en retard plutôt qu'à refaire)
+    * Favoriser le respect entre chacun et être ouvert aux critiques
     * Ne pas demander de rémunération : le fansub est une activité enrichissante mais bénévole
 
 
@@ -33,21 +31,23 @@
 
 [title=2|number]Poster sa fiche de candidature[/title]
 Elle se poste [url=http://forum.zerofansub.net/f21-RECRUTEMENT-Entrer-dans-la-team-de-fansub.htm]sur le forum[/url] (créer un [url=http://forum.zerofansub.net/posting.php?mode=newtopic&f=21]nouveau sujet[/url]). Elle doit être correctement remplie.
-[code]
-[b]Rôle :[/b] 
-[b]Pseudo ou prénom :[/b]
-[b]Âge :[/b] 
-[b]Lieu :[/b] 
-[b]Motivation :[/b] 
-[b]Expérience fansub :[/b] 
-[b]Expérience hors fansub  :[/b] 
-[b]Disponibilités :[/b] 
-[b]Déjà membres d'autre équipe :[/b] 
-[b]Si oui, lesquelles :[/b] 
-[b]Connexion internet :[/b] 
-[b]Système d'exploitation :[/b] 
-[b]Autre chose à dire :[/b] 
-[/code]
+
+[code][b]Nom :[/b][/code] [i]si je veux mieux me faire connaître[/i]
+[code][b]Âge :[/b][/code] [i]si je veux mieux me faire connaître[/i]
+[code][b]Lieu :[/b][/code] [i]si je veux mieux me faire connaître[/i]
+[code][b]Rôle :[/b][/code] [i]le ou les rôles ci-dessus que je veux faire[/i]
+[code][b]Motivation :[/b][/code] [i]pourquoi je veux faire ce rôle et pourquoi chez vous[/i]
+[code][b]Expérience fansub :[/b][/code] [i]ce que j'ai déjà fait dans une team de fansub en rapport avec ce rôle[/i]
+[code][b]Expérience hors fansub  :[/b][/code] [i]ce que j'ai déjà fait dans la vie qui peut m'aider à faire ce rôle[/i]
+[code][b]Disponibilités :[/b][/code] [i]périodes pendant lesquelles je pense pouvoir tenir mon rôle[/i]
+[code][b]Déjà membres d'autre équipe :[/b][/code] [i]si oui lesquelles[/i]
+[code][b]Connexion internet :[/b][/code] [i]connexion continue ou ponctuelle, petit ou gros débit, etc.[/i]
+[code][b]Système d'exploitation :[/b][/code] [i]Windows, Mac, Linux, etc.[/i]
+[code][b]Autre chose à dire :[/b][/code] [i]si je pense que ça peut permettre à la team de mieux juger si ce poste est fait pour moi[/i]
+
+[justify]
+Notre but est de savoir avant tout ce que vous voulez faire, comment vous pensez le faire et de voir si on peut bosser ensemble. Si vous avez des conditions à poser, faites-le dès que vous candidatez. Si vous ne savez pas quelque chose et pensez que ça peut avoir une influence, faites-le savoir. Si votre profil ne correspond pas à notre façon de faire, on ne vous acceptera pas, mais c'est toujours mieux que de perdre notre temps à former quelqu'un qui ne reste pas et surtout de vous faire perdre votre temps alors que vous pourriez postuler dans une team qui vous correspond mieux.
+[/justify]
 
 [title=2|number]Effectuer un test[/title]
 [justify]
@@ -56,30 +56,24 @@ Un test peut être mis à votre  disposition afin d'évaluer un peu votre niveau
 
 [title=2|number]Attendre la réponse des membres de l'équipe[/title]
 [justify]
-Les membres de l'équipe et les administrateurs observeront la fiche et le test et jugeront de l'acceptation ou pas. Rien n'est dit en privé. Toutes les discussions à propos de la candidature seront visibles sur le topic de recrutement, donc pas d'inquiétude :)
-L'acceptation définitive est normalement donnée par le manager ou une personne qualifiée.
-
-[b]En cas de refus[/b]
-Cela peut être pour un niveau insuffisant, pour un manque de disponibilité, etc.
-Ce n'est pas la peine de s'énerver ! Nous expliquons toujours les raisons d'un refus et il n'y a pas de limite de demande. Nous répondrons à toutes les questions, et vous pourrez revenir postuler si vous sentez que votre niveau s'est amélioré.
+Les membres de l'équipe et les administrateurs observeront la fiche et le test et jugeront de l'acceptation ou pas. Rien n'est dit en privé. Toutes les discussions à propos de la candidature seront visibles sur le topic de recrutement, donc pas d'inquiétude :). L'acceptation définitive est normalement donnée par celui qui prendra en charge la formation.
 [/justify]
-
+[b]En cas de refus[/b]
+[justify]
+Cela peut être pour un niveau insuffisant, pour un manque de disponibilité, etc. Ce n'est pas la peine de s'énerver ! Nous expliquons toujours les raisons d'un refus et il n'y a pas de limite de demande. Nous répondrons à toutes les questions, et vous pourrez revenir postuler si vous sentez que votre niveau s'est amélioré.
+[/justify]
 [b]En cas d'acceptation[/b]
 
 - S'il n'est pas déjà donné, réclamer l'accès à la partie privée. 
 - S'il n'est pas déjà donné, réclamer l'accès à la série sur laquelle vous allez travailler. 
 - Lire les tuto qui vous concernent directement ou indirectement et les appliquer. 
-- Se tenir au courant des dernières tendances de l'équipe.
-- Créer son topic dans la partie \"Messages privés-publics\".
+- Créer (et suivre) son topic dans la partie \"Messages privés-publics\".
+- Suivre les topics principaux et ceux qui vous concernent directement.
 - Commencer à faire son boulot. Ne pas hésiter à poser moult questions !
 
 [title=2]Trucs et astuces pour être bien vu[/title]
 [justify]
-Plus la fiche de candidature est soignée, plus on aura une belle image de vous. 
-L'orthographe, c'est très important. La façon de parler aussi. 
-
-Plus votre candidature sera longue, complète et argumentée, mieux ce sera. 
-Nous prendrons le temps de tout lire !
+Plus la fiche de candidature est soignée, plus vous montrerez que vous n'êtes pas là juste pour tuer le temps. L'orthographe, c'est très important. La façon de parler aussi. Plus votre candidature sera longue, complète et argumentée, mieux ce sera. Nous prendrons le temps de tout lire !
 [/justify]
 [/left]
 ")));
