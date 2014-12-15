e010d20f55ffc1e8c6a07c718693021916769696
Sazaju HITOKAGE <sazaju@gmail.com>
2014-12-16 00:31:46 +0100

    Prépare la news pour la sortie de Genshiken.

--- class/model/News.php
+++ class/model/News.php
@@ -5000,6 +5000,49 @@ Pour ceux qui s'en souviennent et on aimé, peut-être savez-vous que la Zéro s
 			$news->addReleasing(Release::getRelease('kanamemobook', 'ch2'));
 			News::$allNews[] = $news;
 			
+			$news = new News();
+			$news->setTitle("Le retour de Genshiken 2, mais en BD 720p et softsub !");
+			//$news->setPublicationTime(strtotime("2014-12-25 00:00"));
+			$news->addAuthor(TeamMember::getMemberByPseudo('Praia'));
+			$news->setMessage("La sortie des blu-ray nous a donné l'occasion de nous replonger dans cette série
+qui nous tient à cœur.
+
+Comme on ne fait pas les choses à moitié, tout a été revu.
+
+Et je dois dire que la traduction méritait vraiment d'être revue :
+on avait apparemment mal choisit notre teamUS.
+
+De plus, l'épisode 10 et 11 sont cette fois-ci sans écriture jap.
+
+Le point négatif, c'est qu'il n'ya pas de 1080p.
+Les BD sont probablement des pseudo BD, vu que la série date un peu.
+
+Vous avez constaté notre changement de politique au softsub,
+j'espère que ce sera vu comme un point positif.
+
+Petite parenthèse : il y a deux histoires avant Nidaime
+qui n'ont pas été adaptées en anime :
+
+- le camp d'entraînement
+- le passé d'Ogiue
+
+Pas d'annonce d'adaptation d'OAV à ce jour ^_^
+
+[img=images/news/genshiken2_fini.png]Version BD dans les bacs ![/img]
+
+P.S. : Vous vous demandez sûrement pourquoi on n'a pas sorti Kiss X Sis OAD.
+Le projet est en pause le temps que notre trad jap retrouve un bon rythme.
+
+Sur ce, joyeux Noël !");
+			// TODO $news->setCommentId(331);
+			$news->setDisplayInNormalMode(true);
+			$news->setDisplayInHentaiMode(false);
+			$news->setTeamNews(false);
+			$news->setPartnerNews(false);
+			$news->setDb0CompanyNews(false);
+			$news->addReleasing(Project::getProject('genshiken'));
+			News::$allNews[] = $news;
+			
 			$ids = array();
 			foreach(News::$allNews as $news) {
 				$id = $news->getId();
