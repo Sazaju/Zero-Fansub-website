483b22c10eb6b6f1d071d04648cc57b911bb6dce
Sazaju HITOKAGE <sazaju@gmail.com>
2015-04-03 04:58:00 +0200

    Ajoute le tri des dossiers/chansons.

--- radio/index.php
+++ radio/index.php
@@ -129,6 +129,12 @@ class SongDir {
 				// not a song file
 			}
 		}
+		usort($this->songs, function ($a, $b) {
+			return strnatcmp($a->getTitle(), $b->getTitle());
+		});
+		usort($this->subdirs, function ($a, $b) {
+			return strnatcmp($a->getPath(), $b->getPath());
+		});
 	}
 	
 	public function getSongs() {
